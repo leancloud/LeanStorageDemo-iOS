4b46af45a16ee6882d52edd38a53f40d61a16ef2
lzwjava <lzwjava@gmail.com>
2015-08-20 18:38:29 +0800

    add server date & network timeout demo

--- LeanStorageDemo/OtherBasic.m
+++ LeanStorageDemo/OtherBasic.m
@@ -10,4 +10,25 @@
 
 @implementation OtherBasic
 
+- (void)demoGetServerDate {
+    [AVOSCloud getServerDateWithBlock:^(NSDate *date, NSError *error) {
+        [self log:@"服务器时间为：%@", date];
+    }];
+}
+
+- (void)demoConfigNetworkTimeout {
+    NSTimeInterval timerInterval = [AVOSCloud networkTimeoutInterval];
+    [AVOSCloud setNetworkTimeoutInterval:0.01];
+    [AVOSCloud getServerDateWithBlock:^(NSDate *date, NSError *error) {
+        if ([error.domain isEqualToString:@"NSURLErrorDomain"]) {
+            [self log:@"因为设置了 0.01 秒超时，所以该请求超时了。"];
+        } else {
+            [self log:@"%@", error];
+        }
+        [AVOSCloud setNetworkTimeoutInterval:timerInterval];
+    }];;
+}
+
+MakeSourcePath
+
 @end
