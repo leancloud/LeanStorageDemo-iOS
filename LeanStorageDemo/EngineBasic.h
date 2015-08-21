ab91b12f406e651feb6a89f4e4cfcc29c89a6def
lzwjava <lzwjava@gmail.com>
2015-08-21 15:16:30 +0800

    add engine basic

---
+++ LeanStorageDemo/EngineBasic.h
@@ -0,0 +1,13 @@
+//
+//  EngineBasic.h
+//  LeanStorageDemo
+//
+//  Created by lzw on 15/8/21.
+//  Copyright (c) 2015年 AVOS. All rights reserved.
+//
+
+#import "Demo.h"
+
+@interface EngineBasic : Demo
+
+@end
---
+++ LeanStorageDemo/EngineBasic.m
@@ -0,0 +1,76 @@
+//
+//  EngineBasic.m
+//  LeanStorageDemo
+//
+//  Created by lzw on 15/8/21.
+//  Copyright (c) 2015年 AVOS. All rights reserved.
+//
+
+#import "EngineBasic.h"
+/*!
+ *  云引擎与 SDK 的交互
+ */
+@implementation EngineBasic
+
+
+- (void)demoCallCloudFunction {
+    [AVCloud callFunctionInBackground:@"hello" withParameters:nil block:^(id object, NSError *error) {
+        if ([self filterError:error]) {
+            [self log:@"云引擎返回结果：%@", object];
+        }
+    }];
+}
+
+- (void)demoErrorCode_ {
+    [AVCloud callFunctionInBackground:@"errorCode" withParameters:nil block:^(id object, NSError *error) {
+        if (!object && error && error.code == 211) {
+            [self log:@"云引擎返回的 Error ：%@", error];
+        }
+    }];
+}
+
+- (void)demoCustomErrorCode_ {
+    NSError *error;
+    id response = [AVCloud callFunction:@"customErrorCode" withParameters:nil error:&error];
+    if (!response && error && error.code == 123) {
+        [self log:@"云引擎返回的 Error ：%@", error];
+    }
+}
+
+-(void)demoFetchObject {
+    [self createStudentForDemo:^(Student *student) {
+        Student *fetchStudent = [Student objectWithoutDataWithClassName:student.className objectId:student.objectId];
+        NSDictionary *params=@{@"obj":fetchStudent};
+        [AVCloud callFunctionInBackground:@"fetchObject" withParameters:params block:^(id object, NSError *error) {
+            if ([self filterError:error]) {
+                [self log:@"云引擎返回的结果：%@", object];
+                [fetchStudent objectFromDictionary:object];
+                
+                [self log:@"根据返回结果构造的对象：%@", fetchStudent];
+            }
+        }];
+    }];
+}
+
+-(void)demoFetchFullObject {
+    [AVCloud callFunctionInBackground:@"fullObject" withParameters:nil block:^(id object, NSError *error) {
+        if ([self filterError:error]) {
+            [self log:@"从云代码中获取整个对象：%@", object];
+        }
+    }];
+}
+
+- (void)demoBeforeSave {
+    AVObject *object = [AVObject objectWithClassName:@"AVCloudTest"];
+    object[@"string"] = @"This is too much long, too much long, too long";
+    object.fetchWhenSave = YES; // 保存的时候也获取最新属性
+    [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if ([self filterError:error]) {
+            [self log:@"通过 beforeSave Hook 截断至 10个字符：%@", object[@"string"]];
+        }
+    }];
+}
+
+MakeSourcePath
+
+@end
