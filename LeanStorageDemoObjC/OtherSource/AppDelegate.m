568746cf032d6eabc24624d589254b7bb67293f1
xsui <284898541@qq.com>
2020-10-10 17:30:37 +0800

    upgrade objc sdk to v12.3.0

--- LeanStorageDemoObjC/OtherSource/AppDelegate.m
+++ LeanStorageDemoObjC/OtherSource/AppDelegate.m
@@ -18,6 +18,7 @@
 #warning 请替换成自己的id和key，来查看自己的后台数据
 #define AVOSCloudAppID  @"G3ROSsF7zRSA3vHD5zw7mDv1"
 #define AVOSCloudAppKey @"xx0gLOhJsksq4DauasmJrTGf"
+#define AVOSCloudServerUrl @"https://g3rossf7.lc-cn-n1-shared.com"
 
 @implementation AppDelegate
 
@@ -29,36 +30,13 @@
     
     //设置AVOSCloud
     [AVOSCloud setApplicationId:AVOSCloudAppID
-                      clientKey:AVOSCloudAppKey];
-    
-    //统计应用启动情况
-    [AVAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
-    
-    
-    /**可选 统计应用崩溃数据
-     *
-     * 其他可用方法:
-     *
-     * * 捕获异常, 并询问用户是否忽略, 可以使应用保持在运行状态而不闪退.
-     * [AVAnalytics setCrashReportEnabled:(BOOL) andIgnore:(BOOL)]
-     *
-     * * 功能同上, 但是可以自定义询问用户的文字
-     * [AVAnalytics setCrashReportEnabled:(BOOL) withIgnoreAlertTitle:(NSString *)
-     *    andMessage:(NSString *) andQuitTitle:(NSString *) andContinueTitle:(NSString *)]
-     */
-    //[AVAnalytics setCrashReportEnabled:YES];
-    
-    //可选 打开推送功能 因为推送只支持真机上, 我们可以加个编译时判断
-    //同样会防止在模拟器运行时得到`didFailToRegisterForRemoteNotificationsWithError`的错误提醒
-    
+                      clientKey:AVOSCloudAppKey serverURLString:AVOSCloudServerUrl];
 #if !TARGET_IPHONE_SIMULATOR
     [AVOSCloud registerForRemoteNotification];
 #endif
-    
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = [self rootController];
     [self.window makeKeyAndVisible];
-    
     
     // 输出内部日志，发布时记得关闭
 #ifdef DEBUG
@@ -70,19 +48,15 @@
 
 - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     
-    //推送功能打开时, 注册当前的设备, 同时记录用户活跃, 方便进行有针对的推送
-    AVInstallation *currentInstallation = [AVInstallation currentInstallation];
-    [currentInstallation setDeviceTokenFromData:deviceToken];
-    
-    //可选 但是很重要. 我们可以在任何地方给currentInstallation设置任意值,方便进行有针对的推送
-    //比如如果我们知道用户的年龄了,可以加上下面这一行 这样推送时我们可以选择age>20岁的用户进行通知
-    //[currentInstallation setObject:@"28" forKey:@"age"];
-    
-    //我们当然也可以设置根据地理位置提醒 发挥想象力吧!
-    
-    
-    //当然别忘了任何currentInstallation的变更后做保存
-    [currentInstallation saveInBackground];
+    [[AVInstallation defaultInstallation] setDeviceTokenFromData:deviceToken
+                                                          teamId:@"7J5XFNL99Q"];
+    [[AVInstallation defaultInstallation] saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            // save succeeded
+        } else if (error) {
+            NSLog(@"%@", error);
+        }
+    }];
 }
 
 -(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
